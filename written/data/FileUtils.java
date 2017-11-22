f2db485bd826788eea8349e57775d1dc4b558e00
Benedikt Schlagberger <schlagbe@in.tum.de>
2017-11-22 18:35:03 +0100

    wip: Adding preliminaries, moving requirements to introduction and preliminaries

---
+++ written/data/FileUtils.java
@@ -0,0 +1,25 @@
+public static String getDataColumn(Context context, Uri uri, String selection,
+									   String[] selectionArgs) {
+
+		Cursor cursor = null;
+		final String column = "_data";
+		final String[] projection = {
+				column
+		};
+
+		try {
+			cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs,null);
+			if (cursor != null && cursor.moveToFirst()) {
+				final int column_index = cursor.getColumnIndexOrThrow(column);
+				return cursor.getString(column_index);
+			}
+		} catch(Exception e) {
+			return null;
+		} finally {
+			if (cursor != null) {
+				cursor.close();
+			}
+		}
+		return null;
+	}
+
---
+++ written/data/MusicPlayerRemote.java
@@ -0,0 +1,24 @@
+private static String getFilePathFromUri(Context context, Uri uri)
+    {
+        Cursor cursor = null;
+        final String column = "_data";
+        final String[] projection = {
+                column
+        };
+
+        try {
+            cursor = context.getContentResolver().query(uri, projection, null, null,
+                    null);
+            if (cursor != null && cursor.moveToFirst()) {
+                final int column_index = cursor.getColumnIndexOrThrow(column);
+                return cursor.getString(column_index);
+            }
+        } catch (Exception e) {
+            Log.e(TAG, e.getMessage());
+        } finally {
+            if (cursor != null)
+                cursor.close();
+        }
+        return null;
+    }
+
